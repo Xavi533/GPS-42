8b5e9621429eb41bf38661a178795cfbec04aad1
Xavi533 <34425101+Xavi533@users.noreply.github.com>
2024-12-19 15:23:25 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,25 @@
+
 import unittest
 import transform
 
 
 class TestStringMethods(unittest.TestCase):
+    """Clase de pruebas unitarias para las funciones de transformación de cadenas."""
 
     def test_is_upper(self):
-        sting = transform.to_upper_case("hello")
-        self.assertEqual(sting, "HELLO")
+        """Prueba si la función to_upper_case transforma correctamente el texto a mayúsculas."""
+        string = transform.to_upper_case("hello")
+        self.assertEqual(string, "HELLO")
 
     def test_is_lower(self):
-        sting = transform.to_lower_case("HELLO")
-        self.assertEqual(sting, "hello")
+        """Prueba si la función to_lower_case transforma correctamente el texto a minúsculas."""
+        string = transform.to_lower_case("HELLO")
+        self.assertEqual(string, "hello")
 
     def test_is_capitalize(self):
-        sting = transform.to_capitalize("HELLO")
-        self.assertEqual(sting, "Hello")
+        """Prueba si la función to_capitalize transforma correctamente el texto a capitalizado."""
+        string = transform.to_capitalize("HELLO")
+        self.assertEqual(string, "Hello")
 
 
 if __name__ == '__main__':
